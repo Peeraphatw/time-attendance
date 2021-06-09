11d5049c30e0b30b847e85e4435a4d169c883ef9
Peeraphatw <judiciarypz>
2021-06-09 21:06:21 +0700

    add new route

---
+++ pages/product.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import MyListbox from './components/Selection';
+function Product() {
+  return (
+    <div className='mx-auto w-11/12 md:w-5/12'>
+      <MyListbox></MyListbox>
+      <div className='grid mt-5 gap-y-2 grid-cols-1'>
+        <div className='__card-1 shadow-sm rounded-md border border-gray-300 p-1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          aperiam, repellat minima nam necessitatibus saepe, dignissimos vero
+          dicta autem, ut libero itaque similique deleniti est? Mollitia, ut
+          non! Officia, eaque?
+        </div>
+        <div className='__card-1 shadow-sm rounded-md border border-gray-300 p-1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          aperiam, repellat minima nam necessitatibus saepe, dignissimos vero
+          dicta autem, ut libero itaque similique deleniti est? Mollitia, ut
+          non! Officia, eaque?
+        </div>
+        <div className='__card-1 shadow-sm rounded-md border border-gray-300 p-1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          aperiam, repellat minima nam necessitatibus saepe, dignissimos vero
+          dicta autem, ut libero itaque similique deleniti est? Mollitia, ut
+          non! Officia, eaque?
+        </div>
+        <div className='__card-1 shadow-sm rounded-md border border-gray-300 p-1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          aperiam, repellat minima nam necessitatibus saepe, dignissimos vero
+          dicta autem, ut libero itaque similique deleniti est? Mollitia, ut
+          non! Officia, eaque?
+        </div>
+        <div className='__card-1 shadow-sm rounded-md border border-gray-300 p-1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          aperiam, repellat minima nam necessitatibus saepe, dignissimos vero
+          dicta autem, ut libero itaque similique deleniti est? Mollitia, ut
+          non! Officia, eaque?
+        </div>
+        <div className='__card-1 shadow-sm rounded-md border border-gray-300 p-1'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          aperiam, repellat minima nam necessitatibus saepe, dignissimos vero
+          dicta autem, ut libero itaque similique deleniti est? Mollitia, ut
+          non! Officia, eaque?
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
