f8894c60d763c4b02d08f712f0910252a657e041
Peeraphatw <judiciarypz>
2021-06-10 17:59:08 +0700

    pic

--- pages/Layout/index.jsx
+++ pages/Layout/index.jsx
@@ -81,7 +81,7 @@ function Index({ children }) {
           href='logo'
           className='text-2xl font-bold text-[#6D2B83] flex-shrink-0 mt-2'
         >
-          <img src='/logo.png' width='65' height='65' alt='logo' />
+          <img src='/Logo.png' width='65' height='65' alt='logo' />
         </a>
         <nav className='hidden sm:hidden md:flex w-full mt-1 '>
           {lists.map((item, index) => {
