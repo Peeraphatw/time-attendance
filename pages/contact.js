1c5e3e33974db5b9c04ef6ee5c7f7da9d5dcde3a
Peeraphatw <judiciarypz>
2021-06-10 18:00:16 +0700

    test

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt
+      aperiam eveniet nesciunt eum praesentium quaerat dolores, asperiores
+      repudiandae consequatur totam, similique non necessitatibus laborum, vero
+      quibusdam nemo tempore minus facilis beatae id dignissimos nulla porro!
+      Officia maxime non est neque odit reprehenderit aliquam laudantium, iusto
+      sequi, mollitia deserunt, velit cupiditate dolore illo! Nobis fugit
+      reiciendis eligendi eum repudiandae dolorem quas omnis. Veniam eum
+      accusamus nihil consectetur repellat at laboriosam perspiciatis laudantium
+      reiciendis magnam, optio amet ex enim tempore corporis fugit nisi? Libero
+      itaque ipsam repudiandae possimus optio cum porro tempora, molestias,
+      recusandae quis sit necessitatibus tempore. Alias, debitis minima illum
+      quasi officiis eum officia hic, a at eaque quo aliquid dicta labore
+      dolorem natus neque necessitatibus quidem mollitia dolor ad perspiciatis
+      pariatur laboriosam blanditiis? Necessitatibus dolore nesciunt reiciendis
+      iusto ab quae, modi dolor placeat iste ducimus aperiam quod distinctio
+      laboriosam officiis molestiae blanditiis facilis voluptate quisquam et
+      nostrum nemo alias voluptatibus accusantium. Dolorem aspernatur ex ad
+      dolor, aperiam porro cum amet? Molestiae adipisci ab repellat aperiam
+      excepturi, quod doloribus id soluta ea. Facere iste est debitis recusandae
+      repudiandae, id perferendis omnis rerum, similique aut porro ratione totam
+      tempore magni consectetur amet cupiditate delectus non doloremque iure
+      labore numquam dolorem.
+    </div>
+  );
 }
 
 export default Contact;
